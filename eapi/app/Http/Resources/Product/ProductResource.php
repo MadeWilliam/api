61b81c340138cc596c93f8571a651df8cf833e7f
MadeWilliam <pupul.mwm@gmail.com>
2019-10-08 02:31:35 +0700

    Modified Product Details

--- eapi/app/Http/Resources/Product/ProductResource.php
+++ eapi/app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => (!$this->stock) ? 'Out of stock' : $this->stock ,
+            'discount' => $this->discount,
+            'totalPrice' => round(( 1 - ($this->discount / 100) ) * $this->price,2),
+            'rating' => ($this->reviews->count()) ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
